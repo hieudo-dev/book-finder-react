b3a2684fb23f442138cfd8d3d364c100dc926d54
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-06-13 01:02:03 -0400

    feat: implement searching functionality

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,49 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { SearchIcon } from '../icons';
+import axios from 'axios';
 
-const BookSummary = ({ title }) => {
+const BookSummary = ({ title, authors, description, thumbnail, link }) => {
   return (
-    <div
-      className="flex md:block items-center bg-white p-4 my-2 md:mx-3 rounded-lg
+    <a href={link}>
+      <div
+        className="flex flex-row items-center bg-white p-2 md:py-8 my-2 md:mx-3 rounded-lg
       transition-transform duration-300 ease-out transform hover:scale-105"
-    >
-      <img
-        className="w-20 md:w-40 md:w-full md:mb-2 object-contain object-center"
-        src="https://dummyimage.com/723x403"
-        alt="content"
-      />
-      <div className="block ml-4 md:ml-2">
-        <div className="tracking-widest text-indigo-500 text-xs font-medium title-font truncate">
-          Author
+      >
+        <img
+          className="w-20 h-20 md:w-32 md:h-32 md:mb-2 object-scale-down object-center"
+          src={thumbnail ? thumbnail : 'https://dummyimage.com/723x403'}
+          alt="content"
+        />
+        <div className="flex flex-col ml-2 w-full overflow-hidden">
+          <div className="tracking-widest text-indigo-500 text-xs font-medium title-font overflow-hidden truncate">
+            {authors.length !== 1 ? authors.join(', ') : authors}
+          </div>
+          <div className="text-xs md:text-base text-gray-900 font-medium title-font mb-2 truncate">
+            {title}
+          </div>
+          <p className="relative h-24 overflow-hidden text-xs md:text-base">
+            <span
+              className="absolute"
+              style={{
+                width: '100%',
+                height: '110%',
+                background:
+                  'linear-gradient(to bottom, rgba(255,255,255,0),#ffffff)',
+              }}
+            />
+            {description ? (
+              description
+            ) : (
+              <div className="text-xs text-gray-500 italic">No description</div>
+            )}
+          </p>
         </div>
-        <div className="text-sm md:text-base text-gray-900 font-medium title-font mb-2 truncate">
-          {title}
-        </div>
-        <p className="relative h-24 overflow-hidden text-sm md:text-base">
-          <span
-            className="absolute"
-            style={{
-              width: '100%',
-              height: '110%',
-              background:
-                'linear-gradient(to bottom, rgba(255,255,255,0),#ffffff)',
-            }}
-          />
-          Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-          hexagon disrupt edison bulbche. Fingerstache flexitarian street art
-          8-bit waistcoat. Distillery hexagon disrupt edison bulbche.
-          Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-          hexagon disrupt edison bulbche. Fingerstache flexitarian street art
-          8-bit waistcoat. Distillery hexagon disrupt edison bulbche.
-        </p>
       </div>
-    </div>
+    </a>
   );
 };
 
 const BookList = () => {
-  const arr = [...Array(5).keys()];
+  const [books, setBooks] = useState([]);
+  const [keywords, setKeywords] = useState([]);
+
+  const handleSearch = () => {
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+          keywords
+        )}`
+      )
+      .then((response) => {
+        setBooks(
+          response.data.items.map((volume) => ({
+            ...volume.volumeInfo,
+            id: volume.id,
+          }))
+        );
+      });
+  };
 
   return (
     <section>
@@ -56,23 +76,32 @@ const BookList = () => {
           transition-colors duration-300 hover:bg-gray-700"
             placeholder="Search books..."
             autoComplete="off"
+            onChange={({ target: { value } }) => setKeywords(value)}
           />
           <span className="absolute inset-y-0 right-0 flex items-center">
             <button
               type="submit"
               className="p-3 focus:outline-none focus:shadow-outline"
+              onClick={() => handleSearch()}
             >
               <SearchIcon className="w-6 h-6" />
             </button>
           </span>
         </div>
       </form>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-center gap-3 pt-12">
-        {arr.map((e) => (
-          <motion.div key={e} positionTransition>
-            <BookSummary title={e} />
-          </motion.div>
-        ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-3 pt-12">
+        {books.length !== 0 &&
+          books.map((book) => (
+            <motion.div key={book.id} positionTransition>
+              <BookSummary
+                title={book.title}
+                authors={book.authors}
+                description={book.description}
+                thumbnail={book.imageLinks?.thumbnail}
+                link={book.infoLink}
+              />
+            </motion.div>
+          ))}
       </div>
     </section>
   );
