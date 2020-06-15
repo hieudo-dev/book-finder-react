ded294040d671dca73040ee7e435e9fdb478c267
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-06-15 15:20:16 -0400

    feat: improve book's UI and show published date

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -5,45 +5,59 @@ import axios from 'axios';
 import _ from 'lodash';
 import PageNavigation from './pageNavigation';
 
-const BookSummary = ({ title, authors, description, thumbnail, link }) => {
+const BookSummary = ({
+  title,
+  authors,
+  description,
+  date,
+  thumbnail,
+  link,
+}) => {
   return (
     <a href={link}>
       <div
-        className="flex flex-row items-center bg-white p-2 md:py-4 my-2 mx-2 rounded-lg
+        className="bg-white p-2 md:py-4 sm:m-1 rounded-lg
       transition-transform duration-300 ease-out transform hover:scale-105"
       >
-        <img
-          className="w-20 h-20 md:w-32 md:h-32 md:mb-2 object-scale-down object-center"
-          src={thumbnail ? thumbnail : 'https://dummyimage.com/723x403'}
-          alt="content"
-        />
-        <div className="flex flex-col ml-2 w-full overflow-hidden">
-          <div className="tracking-widest text-indigo-500 text-xs font-medium title-font overflow-hidden truncate">
-            {!authors?.length ? (
-              <div className="italic">No authors</div>
-            ) : (
-              authors.join(', ')
-            )}
+        <div className="ml-2 sm:ml-6 mb-1 sm:mb-2 text-sm md:text-base text-gray-900 font-medium title-font truncate">
+          {title}
+        </div>
+        <div className="flex flex-row items-center">
+          <img
+            className="w-20 h-20 md:w-32 md:h-32 md:mb-2 object-scale-down object-center"
+            src={thumbnail ? thumbnail : 'https://dummyimage.com/723x403'}
+            alt="content"
+          />
+          <div className="flex flex-col ml-2 w-full overflow-hidden">
+            <div className="tracking-widest text-indigo-500 text-xs font-medium title-font overflow-hidden truncate">
+              {!authors?.length ? (
+                <div className="italic">No authors</div>
+              ) : (
+                authors.join(', ')
+              )}
+            </div>
+            <div className="text-gray-900 text-xs font-medium overflow-hidden truncate">
+              {date ? <span>{date}</span> : <span />}
+            </div>
+            <p className="relative h-24 md:h-32 overflow-hidden text-xs md:text-sm">
+              <span
+                className="absolute"
+                style={{
+                  width: '100%',
+                  height: '110%',
+                  background:
+                    'linear-gradient(to bottom, rgba(255,255,255,0),#ffffff)',
+                }}
+              />
+              {description ? (
+                description
+              ) : (
+                <div className="text-xs text-gray-500 italic">
+                  No description
+                </div>
+              )}
+            </p>
           </div>
-          <div className="text-xs md:text-base text-gray-900 font-medium title-font mb-2 truncate">
-            {title}
-          </div>
-          <p className="relative h-24 md:h-32 overflow-hidden text-xs md:text-base">
-            <span
-              className="absolute"
-              style={{
-                width: '100%',
-                height: '110%',
-                background:
-                  'linear-gradient(to bottom, rgba(255,255,255,0),#ffffff)',
-              }}
-            />
-            {description ? (
-              description
-            ) : (
-              <div className="text-xs text-gray-500 italic">No description</div>
-            )}
-          </p>
         </div>
       </div>
     </a>
@@ -127,6 +141,7 @@ const BookList = () => {
                   title={book.title}
                   authors={book.authors}
                   description={book.description}
+                  date={book?.publishedDate}
                   thumbnail={book.imageLinks?.thumbnail}
                   link={book.infoLink}
                 />
